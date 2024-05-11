dff3ba8dbbb825ec9ae7dd753a53f2456ae4bd70
LAPTOP-KLF3DVEL\santa <santanupaikaray1996@gmail.com>
2024-05-11 22:19:43 +0530

    add all chnages

---
+++ data_type.py
@@ -0,0 +1,24 @@
+# var_1 = 0*123
+# var_2 = 0*123
+# print(var_1)
+# print(var_2)
+# name = "Santanu Paikaray"
+# print(type(name))
+# name = "Santanu Paikaray"
+# print(name[3])
+# name_1 = "Santanu Paikaray"
+# name_2 = " CS Faculty"
+# a=1
+# print(name_1 + a)
+# name_1= "123"
+# name_2= "1"
+# a=1
+# print(name_1 + name_2)
+# var = True
+# print(var)
+# print(type(var))
+a=1
+b=2
+var=a<b
+print(var)
+print(type(var))
---
+++ type_casting.py
@@ -0,0 +1,7 @@
+# print(len("jenny khatri"))
+# length=len("Santanu Paikaray")
+# print("Your name has " +str(length) + " characters")
+first_number=input("Enter First Number: ")
+second_number=input("Enter Second Number: ")
+sum=int(first_number) + int(second_number)
+print(sum)
